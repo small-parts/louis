67b6b215864b55ebd0a240dbb80b5099aa5dff14
ltoddy <taoliu0509@gmail.com>
2020-07-30 22:41:18 +0800

    init format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+use std::f64;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::iter;
+
+use structopt::StructOpt;
+
+use crate::options::Options;
+
+pub mod options;
+
+const VERTICAL_DIVIDER: char = '│';
+const HORIZONTAL_DIVIDER: char = '─';
+
 fn main() {
-    println!("Hello, world!");
+    let opt: Options = Options::from_args();
+
+    let Options { entry } = opt;
+
+    let fp = File::open(entry).unwrap();
+    let meta = fp.metadata().unwrap();
+    let file_size = meta.len();
+
+    let address_label_length = "address"
+        .len()
+        .max(f64::log(file_size as f64, 16.0).ceil() as usize);
+    let mut reader = BufReader::new(fp);
+    let mut buffer = Vec::with_capacity(2 << 10);
+    reader.read_to_end(&mut buffer).unwrap();
+    let chunks = buffer.chunks(8).collect::<Vec<_>>();
+
+    println!(
+        "┌{}┬{}┐",
+        iter::repeat(HORIZONTAL_DIVIDER)
+            .take(address_label_length)
+            .collect::<String>(),
+        iter::repeat(HORIZONTAL_DIVIDER)
+            .take(23)
+            .collect::<String>()
+    );
+    println!(
+        "{divider}{address_label:<length$}{divider}{data_label:^23}",
+        divider = VERTICAL_DIVIDER,
+        address_label = "address",
+        length = address_label_length,
+        data_label = "hexadecimal data"
+    );
+
+    for (index, chunk) in chunks.iter().enumerate() {
+        let address = index * 8;
+        print!(
+            "{divider}{address:<0length$x}",
+            divider = VERTICAL_DIVIDER,
+            address = address,
+            length = address_label_length
+        );
+        print!("{}", VERTICAL_DIVIDER);
+        for number in chunk.iter() {
+            print!("{:<02X} ", number);
+        }
+        println!("{}", VERTICAL_DIVIDER);
+    }
+
+    println!(
+        "└{}┴{}┘",
+        iter::repeat(HORIZONTAL_DIVIDER)
+            .take(address_label_length)
+            .collect::<String>(),
+        iter::repeat(HORIZONTAL_DIVIDER)
+            .take(23)
+            .collect::<String>()
+    );
 }
---
+++ src/options.rs
@@ -0,0 +1,13 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "A command-line hex viewer",
+    author = "ltoddy - taoliu0509@gmail.com"
+)]
+pub struct Options {
+    #[structopt(name = "path", parse(from_os_str))]
+    pub entry: PathBuf,
+}
